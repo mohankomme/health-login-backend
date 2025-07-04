95ccbb5e8afb8388fee9dd9774232ce80db444fd
KOMME MOHAN <144981766+mohankomme@users.noreply.github.com>
2025-07-04 13:30:44 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv\config");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch((err) => console.log("❌ DB Error:", err));
+
+app.use("/api/auth", require("./routes/auth"));
+
+app.get("/", (req, res) => {
+  res.send("Health Monitoring API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
